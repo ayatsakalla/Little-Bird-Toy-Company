517809e840aa051fb15129ede52ee3d4f73f37a2
ayatsakalla <ayat.sakalla@outlook.com>
2024-06-12 14:59:16 -0400

    worked on putting imgs into javascript

--- script.js
+++ script.js
@@ -48,35 +48,90 @@ const productItems = [
     age: 'Toddlers',
     biography: 'Soar through imaginative skies with this classic wooden airplane. Handcrafted from sustainable Baltic birch wood with a safe, natural harvest finish and a spinning propeller. Measures 3.5"H x 7"L x 7"W.' ,
     dataIndex: '0',
-    status: 'active'
+    status: 'active',
+    carousel: `<div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                </div>`
   } , 
   {
     productName: 'Wooden Train Set',
     age: '3+', 
     biography: 'Embark on a charming journey with this beautiful handcrafted wooden train set. Engine and three interchangeable cars boast intricate details made from real beech wood. Large size (84cm L x 11cm H x 13cm W) with moving wheels and a fully ecological design.' , 
     dataIndex: '1',
-    status: 'inactive'
+    status: 'inactive',
+    carousel: `<div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                </div>`
   } ,
   {
     productName: 'Wooden Boat',
     age: 'All Ages' ,
     biography: ' Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety. Includes two peg "lobster people." Size: 10.5"W x 3.5"H.' ,
     dataIndex: '2',
-    status: 'inactive'
+    status: 'inactive',
+    carousel: `<div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                </div>`
   } ,
   {
     productName: 'Wooden Block Set',
     age: '3+',
     biography: 'Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate (13” L x 12” W x 2” H).' ,
     dataIndex: '3',
-    status: 'inactive'
+    status: 'inactive',
+    carousel: `<div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                </div>`
   } ,
   {
     productName: 'Wooden Car',
     age: 'All Ages',
     biography: 'This heirloom-quality wooden car is a timeless treasure. Handcrafted from domestic and exotic hardwoods with a clear lacquer finish, this unique car will inspire generations of imaginative play. Please note potential choking hazards for small children.', 
     dataIndex: '4',
-    status: 'inactive'
+    status: 'inactive',
+    carousel: `<div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
+                  </div>
+                </div>`
   }
 ]
 
@@ -98,17 +153,7 @@ function generateProductItems() {
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
                     aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
-                  </div>
-                  <div class="carousel-item">
-                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
-                  </div>
-                  <div class="carousel-item">
-                    <img src="..." class="d-block w-100 img-fluid" alt="..." />
-                  </div>
-                </div>
+                ${product.carousel}
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
                   data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -122,7 +167,7 @@ function generateProductItems() {
               </div>
             </div>
             <div class="article-description-section article-section geliat">
-              ${product.biography}
+             <p>${product.biography}</p>
             </div>
             <div class="article-title-section article-section inaptly">
               <h2>${product.productName}<h2>
